7451bf042fbe86da73ed10eace78c2dcda95dedb
jimmy <mac@macdeAir.home>
2024-01-25 11:50:25 +0800

    task-add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='flex justify-center items-center p-4'>
+            <span>
+                Built with Next14, TypeScript & Tailwind
+            </span>
+        </div>
+    )
+}
