7820a41b8920c605b9849e723e0cf89819e5b941
jimmy <mac@macdeAir.home>
2024-01-25 14:12:37 +0800

    task-revise header list style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
         <div className="text-center p-4">
-            <h1 className="font-bold text-headerMain">Best Seller Gaming PC</h1>
-            <span className="font-bold text-headerSub">Prebuilt & Custom</span>
+            <h1 className="font-bold text-headerMain-mobile  md:text-headerMain">Best Seller Gaming PC</h1>
+            <span className="font-bold text-headerSub-mobile  md:text-headerSub">Prebuilt & Custom</span>
         </div>
     )
 }
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,6 +1,6 @@
 function List({ listData = [] }: { listData: string[] }) {
     return (
-        <ul className='h-36 min-h-36 m-2 overflow-scroll '>
+        <ul className='h-36 min-h-36  overflow-scroll text-base'>
             {listData.length && listData.map((item: string) => <li key={item}>{item}</li>)}
         </ul>
     )
